a92be0ffdb50877a780c38b492d900c1a0effd81
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-11-27 14:48:37 -0500

    Cambios en interfaces y services

---
+++ src/app/interfaces/empleador.ts
@@ -0,0 +1,3 @@
+export default interface IEmpleador {
+    nombre: string;
+  }
--- src/app/services/empleador.services.ts
+++ src/app/services/empleador.services.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import IUsuario from "../interfaces/users";
+import IEmpleador from "../interfaces/empleador";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class UsuariosService {
   constructor(private http: HttpClient) {}
 
-  getUsuarios(): Observable<IUsuario[]> {
-    return this.http.get<IUsuario[]>(
+  getUsuarios(): Observable<IEmpleador[]> {
+    return this.http.get<IEmpleador[]>(
       "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
     );
   }
