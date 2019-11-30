df689d6b568f927aad7ee765327400f4c487b3e5
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-11-29 21:56:59 -0500

    Cambios en los services

--- src/app/services/empleador.services.ts
+++ src/app/services/empleador.services.ts
@@ -1,15 +1,34 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import IEmpleador from "../interfaces/empleador";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class UsuariosService {
+export class EmpleadorService {
   constructor(private http: HttpClient) {}
 
-  getUsuarios(): Observable<IEmpleador[]> {
+  getEmpleador(): Observable<IEmpleador[]> {
     return this.http.get<IEmpleador[]>(
       "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
     );
+  }
+
+  deleteEmpleador(indice: number): Observable<any>{
+    return this.http.delete<any>(
+      `https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${indice}`
+    )
+  }
+
+  createEmpleador(usuario: IEmpleador): Observable<IEmpleador>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+    return this.http.post<IEmpleador>(
+      "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
+      usuario,
+      httpOptions
+    )
   }
 }
--- src/app/services/users.services.ts
+++ src/app/services/users.services.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import IUsuario from "../interfaces/users";
 import { Observable } from "rxjs";
 
@@ -12,4 +12,25 @@ export class UsersServices {
       "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
     );
   }
+
+  deleteUsuario(indice: number): Observable<any>{
+    return this.http.delete<any>(
+      `https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${indice}`
+    );
+  }
+
+  createUsuario(usuario: IUsuario): Observable<IUsuario> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+    return this.http.post<IUsuario>(
+      "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
+      usuario,
+      httpOptions
+    );
+  }
+
+
 }
