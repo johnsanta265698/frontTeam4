7c485b0233d4af75dd86c55932b1daedf7358dd2
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-12-01 18:05:52 -0500

    Cambios en el servicio de empleador

--- src/app/services/empleador.services.ts
+++ src/app/services/empleador.services.ts
@@ -6,28 +6,45 @@ import { Observable } from "rxjs";
 @Injectable()
 export class EmpleadorService {
   constructor(private http: HttpClient) {}
-
+  private _empleador = "localhost:8080/heinsohn-api/rest/v2/empleadores"
   getEmpleador(): Observable<IEmpleador[]> {
     return this.http.get<IEmpleador[]>(
-      "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
+      `${this._empleador}`
+      //"https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios"
     );
   }
 
   deleteEmpleador(indice: number): Observable<any>{
     return this.http.delete<any>(
-      `https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${indice}`
+      `${this._empleador}/${indice}`
+      //`https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${indice}`
     )
   }
 
-  createEmpleador(usuario: IEmpleador): Observable<IEmpleador>{
+  createEmpleador(empleador: IEmpleador): Observable<IEmpleador>{
     const httpOptions = {
       headers: new HttpHeaders({
         "Content-Type": "application/json"
       })
     };
     return this.http.post<IEmpleador>(
-      "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
-      usuario,
+      `${this._empleador}`,
+      //"https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
+      empleador,
+      httpOptions
+    )
+  }
+
+  editEmpleador(indice: number, empleador: IEmpleador): Observable<IEmpleador>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+
+    return this.http.put<IEmpleador>(
+      `${this._empleador}/empleador/${indice}`,
+      empleador,
       httpOptions
     )
   }
